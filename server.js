5d1df08b0563bcb57fc74b0a653dd30135a6a450
KhokhlovAnton <khokhlov.anton@gmail.com>
2018-03-31 01:55:03 +0300

    Mongo

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-
-
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 var indexRouter = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -15,6 +13,7 @@ app.set('view engine', 'jade');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -34,5 +33,24 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// create users
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    var users = [
+        {login: "Admin", pass: '54321'},
+        {login: "User", pass: '12345'},
+        {login: "Guest", pass: 'temp'}
+    ];
+    dbo.collection("users").insertMany(users, function(err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        db.close();
+    });
+});
+
+
+
 
 module.exports = app;
